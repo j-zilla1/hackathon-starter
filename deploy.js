8e799605f5d7afebe5686f6fe0baa4475fb7a5c1
J-mot3000 <70128193+J-mot3000@users.noreply.github.com>
2022-09-28 20:44:11 -0500

    adding .travis.yml and deploy.sh files

--- deploy.js
+++ deploy.js
@@ -1,11 +1,15 @@
-let cmd = require('node-cmd'); 
 
- path, node_ssh, ssh, fs;
+const cmd = require('node-cmd');
 
-fs = require('fs');
+let path; 
+let node_ssh; 
+let ssh; 
+//let fs;
+let fs = require('fs');
 path = require('path');
 node_ssh = require('node-ssh');
-ssh = new node_ssh.NodeSSH(); 
+
+ssh = new node_ssh.NodeSSH();
 
 // the method that starts the deployment process
 function main() {
@@ -15,68 +19,61 @@ function main() {
 
 // installs PM2
 function installPM2() {
-  return ssh.execCommand(
-    'sudo npm install pm2 -g', {
-      cwd: '/home/ubuntu'
+  return ssh.execCommand('sudo npm install pm2 -g', {
+    cwd: '/home/ubuntu'
   });
 }
 
 // transfers local project to the remote server
 function transferProjectToRemote(failed, successful) {
-  return ssh.putDirectory(
-    '../hackathon-starter',
+  return ssh.putDirectory('../hackathon-starter',
     '/home/ubuntu/hackathon-starter-temp',
     {
       recursive: true,
       concurrency: 1,
-      validate: function(itemPath) {
+      validate(itemPath) {
         const baseName = path.basename(itemPath);
         return (
           baseName.substr(0, 1) !== '.' && baseName !== 'node_modules' // do not allow dot files
         ); // do not allow node_modules
       },
-      tick: function(localPath, remotePath, error) {
+      tick(localPath, remotePath, error) {
         if (error) {
           failed.push(localPath);
-          console.log('failed.push: ' + localPath);
+          console.log(`failed.push: ${localPath}`);
         } else {
           successful.push(localPath);
-          console.log('successful.push: ' + localPath);
+          console.log(`successful.push: ${localPath}`);
         }
       }
-    }
-  );
+    });
 }
 
 // creates a temporary folder on the remote server
 function createRemoteTempFolder() {
-  return ssh.execCommand(
-    'rm -rf hackathon-starter-temp && mkdir hackathon-starter-temp', {
-      cwd: '/home/ubuntu'
+  return ssh.execCommand('rm -rf hackathon-starter-temp && mkdir hackathon-starter-temp', {
+    cwd: '/home/ubuntu'
   });
 }
 
 // stops mongodb and node services on the remote server
 function stopRemoteServices() {
-  return ssh.execCommand(
-    'pm2 stop all && sudo service mongod stop', {
-      cwd: '/home/ubuntu'
+  return ssh.execCommand('pm2 stop all && sudo service mongod stop', {
+    cwd: '/home/ubuntu'
   });
 }
 
 // updates the project source on the server
 function updateRemoteApp() {
-  return ssh.execCommand(
-    'mkdir hackathon-starter && cp -r hackathon-starter-temp/* hackathon-starter/ && rm -rf hackathon-starter-temp', {
-      cwd: '/home/ubuntu'
+  return ssh.execCommand('mkdir hackathon-starter && cp -r hackathon-starter-temp/* hackathon-starter/ && rm -rf hackathon-starter-temp', {
+    cwd: '/home/ubuntu'
   });
 }
 
 // restart mongodb and node services on the remote server
 function restartRemoteServices() {
-  return ssh.execCommand(
-    'cd hackathon-starter && sudo service mongod start && pm2 start app.js', {
-      cwd: '/home/ubuntu'
+  return ssh.execCommand('cd hackathon-starter && sudo service mongod start && pm2 start app.js', {
+    cwd: '/home/ubuntu'
   });
 }
 
@@ -87,61 +84,58 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '34.205.125.80',
+      host: '18.212.166.191',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 'hs-key12.pem'
     })
-    .then(function() {
+    .then(() => {
       console.log('SSH Connection established.');
       console.log('Installing PM2...');
       return installPM2();
     })
-    .then(function() {
+    .then(() => {
       console.log('Creating `hackathon-starter-temp` folder.');
       return createRemoteTempFolder();
     })
-    .then(function(result) {
+    .then((result) => {
       const failed = [];
       const successful = [];
       if (result.stdout) {
-        console.log('STDOUT: ' + result.stdout);
+        console.log(`STDOUT: ${result.stdout}`);
       }
       if (result.stderr) {
-        console.log('STDERR: ' + result.stderr);
+        console.log(`STDERR: ${result.stderr}`);
         return Promise.reject(result.stderr);
       }
       console.log('Transferring files to remote server...');
       return transferProjectToRemote(failed, successful);
     })
-    .then(function(status) {
+    .then((status) => {
       if (status) {
         console.log('Stopping remote services.');
         return stopRemoteServices();
-      } else {
-        return Promise.reject(failed.join(', '));
       }
+      return Promise.reject(failed.join(', '));
     })
-    .then(function(status) {
+    .then((status) => {
       if (status) {
         console.log('Updating remote app.');
         return updateRemoteApp();
-      } else {
-        return Promise.reject(failed.join(', '));
       }
+      return Promise.reject(failed.join(', '));
     })
-    .then(function(status) {
+    .then((status) => {
       if (status) {
         console.log('Restarting remote services...');
         return restartRemoteServices();
-      } else {
-        return Promise.reject(failed.join(', '));
       }
+      return Promise.reject(failed.join(', '));
     })
-    .then(function() {
+    .then(() => {
       console.log('DEPLOYMENT COMPLETE!');
       process.exit(0);
     })
-    .catch(e => {
+    .catch((e) => {
       console.error(e);
       process.exit(1);
     });
